db03213e7876eb83f0716d5c5a10cab136af30d4
Rasmus Knudsen <64476703+rfriisk@users.noreply.github.com>
2023-01-17 12:53:24 +0100

    LoginWith2factor // Husk at sætte brakepoint, ved oprettelse af ny bruger

--- BlazorAppTestOgSikkerhed/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BlazorAppTestOgSikkerhed/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -113,6 +113,8 @@ namespace BlazorAppTestOgSikkerhed.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                // Activate with this code HELP
+                user.TwoFactorEnabled = true;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
