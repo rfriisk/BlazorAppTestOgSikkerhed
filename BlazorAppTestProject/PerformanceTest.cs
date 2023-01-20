9d0ec80e1c5830055bb4f32b4492449114a45c83
Rasmus Knudsen <64476703+rfriisk@users.noreply.github.com>
2023-01-20 11:47:48 +0100

    Performance test forsøgt, ikke færdig

---
+++ BlazorAppTestProject/PerformanceTest.cs
@@ -0,0 +1,29 @@
+﻿using Bunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppTestProject
+{
+    public class PerformanceTest
+    {
+        private const int N = 10000;
+
+        [Fact]
+        public void TestPerformance()
+        {
+            // Arrange
+            using var context = new TestContext();
+
+
+
+            // Act
+            var perfTest = context.
+
+            // Assert
+
+        }
+    }
+}
